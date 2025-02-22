b42c7429682a77e1b79acd777bd3156f6d57cfa8
Katheryn Guasca <katheryng2017@gmail.com>
2025-02-22 14:26:37 +0000

    Agregando Clase Tratamiento y Medicamento

---
+++ Proyecto/demo/src/main/java/com/veterinaria/demo/controlador/TratamientoController.java
@@ -0,0 +1,11 @@
+package com.veterinaria.demo.entidad;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tratamiento")
+public class TratamientoController{
+
+
+}
