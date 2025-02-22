16dbdc1d21a6de210905c85fcb3c2a711848f4a0
Katheryn Guasca <katheryng2017@gmail.com>
2025-02-22 15:29:58 +0000

    Modificando Capa controlador

--- Proyecto/demo/src/main/java/com/veterinaria/demo/controlador/MedicamentoController.java
+++ Proyecto/demo/src/main/java/com/veterinaria/demo/controlador/MedicamentoController.java
@@ -2,10 +2,27 @@ package com.veterinaria.demo.entidad;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import com.veterinaria.demo.servicio.MedicamentoService; 
 
 @Controller
 @RequestMapping("/medicamento")
 public class MedicamentoController{
 
-    
+    @Autowired 
+    MedicamentoService medicamentoService;
+
+    /*Pagina para mostrar Medicamentos 
+    @GetMapping ("/all")
+    public String mostrarMedicamentos(){
+        return "mostrarMedicamentos"; 
+    }*/ 
+
+    /*Pagina para buscar medicamentos
+    @GetMapping ("/find")
+    public String buscarInfoMedicamento(){
+        return "mostrarInfoMedicamento"; 
+    }*/
 }
