da0619189ec245b6da9ba63c206f04c8bb1a2bb2
Katheryn Guasca <katheryng2017@gmail.com>
2025-02-25 00:26:47 +0000

    Agregando Cliente Repository

---
+++ Proyecto/demo/src/main/java/com/veterinaria/demo/repositorio/ClienteRepository.java
@@ -0,0 +1,20 @@
+package com.veterinaria.demo.repositorio;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.veterinaria.demo.entidad.Cliente
+
+@Repository
+public class ClienteRepository{
+   /* public Cliente findById(int id){
+        return data.get(id);
+    }
+    
+    public Collection<Cliente> findAll(){
+        return data.values(); 
+    }*/
+}
