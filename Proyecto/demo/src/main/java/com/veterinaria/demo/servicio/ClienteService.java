c8769484ba8d578f310f376fb6044de9ae5bee6e
Katheryn Guasca <katheryng2017@gmail.com>
2025-02-25 00:25:35 +0000

    Agregando Servicios de Cliente

---
+++ Proyecto/demo/src/main/java/com/veterinaria/demo/servicio/ClienteService.java
@@ -0,0 +1,14 @@
+package com.veterinaria.demo.servicio;
+
+import java.util.List;
+
+import com.veterinaria.demo.entidad.Cliente;
+
+public interface ClienteService{
+
+    /*public Cliente SearchById(int id);
+    
+    public List<Cliente> SearchAll(); */
+
+}
+
