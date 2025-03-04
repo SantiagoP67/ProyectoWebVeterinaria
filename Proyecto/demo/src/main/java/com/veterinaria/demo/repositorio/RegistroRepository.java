19c82d3908fbade82d41f614d0e87b05e0b840e9
Dani2044 <castro-df@javeriana.edu.co>
2025-03-04 10:06:42 -0500

    Corrección

--- Proyecto/demo/src/main/java/com/veterinaria/demo/repositorio/RegistroRepository.java
+++ Proyecto/demo/src/main/java/com/veterinaria/demo/repositorio/RegistroRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RegistroRepository extends JpaRepository<Registro, Integer> {
+public interface RegistroRepository extends JpaRepository<Registro, String> {
 }
