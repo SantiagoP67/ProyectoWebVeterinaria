1cfe9a7d65c43d4c620f43ff7f8ef40a31ce8235
Katheryn Guasca <Katheryng2017@gmail.com>
2025-03-05 10:37:42 -0500

    Agregando navegacion

--- Proyecto/demo/src/main/java/com/veterinaria/demo/controlador/AuthController.java
+++ Proyecto/demo/src/main/java/com/veterinaria/demo/controlador/AuthController.java
@@ -3,11 +3,24 @@ package com.veterinaria.demo.controlador;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class AuthController {
+
     @GetMapping("/inicio_sesion")
     public String mostrarPaginaLogin() {
         return "inicio_sesion"; 
+    }
+
+    @GetMapping("/index")
+    public String mostrarPaginaIndex() {
+        return "index";
+    }
+    @GetMapping("/")
+    public String redirigirAIndex() {
+        return "index"; 
+    }
+    @GetMapping("/registro_usuario")
+    public String mostrarPaginaRegistro() {
+        return "registro_usuario"; // Debe existir en src/main/resources/templates/
     }
 }
