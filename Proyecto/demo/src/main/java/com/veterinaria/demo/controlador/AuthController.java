f5e765e340ad1ec63bb5c664561c134dd10c2544
Katheryn Guasca <Katheryng2017@gmail.com>
2025-03-07 08:45:00 -0500

    Agregando Cambios al AuthController para que funcionara

--- Proyecto/demo/src/main/java/com/veterinaria/demo/controlador/AuthController.java
+++ Proyecto/demo/src/main/java/com/veterinaria/demo/controlador/AuthController.java
@@ -1,50 +1,28 @@
 package com.veterinaria.demo.controlador;
 
-import com.veterinaria.demo.entidad.Cliente;
-import com.veterinaria.demo.servicio.ClienteService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
-    @Autowired
-    private ClienteService clienteService;
 
-    @GetMapping("/inicio_sesion")
+    @GetMapping("inicio_sesion")
     public String mostrarPaginaLogin() {
-        return "inicio_sesion";
+        return "inicio_sesion"; 
     }
 
-    @PostMapping("/inicio_sesion")
-    public String validarLogin(@RequestParam("username") String username,
-                               @RequestParam("password") String password,
-                               Model model) {
-
-        Cliente cliente = clienteService.validarCliente(username, password);
-        Integer idCliente = cliente.getIdCliente();                           
-        if (cliente != null) {
-            model.addAttribute("mensaje", "Inicio de sesión exitoso");
-            return "redirect:/mascota/mascotas?idCliente=" + idCliente + ""; // Redirigir a la página principal
-        } else {
-            model.addAttribute("error", "Usuario o contraseña incorrectos");
-            return "inicio_sesion";
-        }
-    }
-
-    @GetMapping("/index")
+    @GetMapping("index")
     public String mostrarPaginaIndex() {
         return "index";
     }
-    @GetMapping("/")
+
+    @GetMapping
     public String redirigirAIndex() {
         return "index"; 
     }
-    @GetMapping("/registro_usuario")
+
+    @GetMapping("registro_usuario")
     public String mostrarPaginaRegistro() {
-        return "registro_usuario";
+        return "registro_usuario"; 
     }
 }
