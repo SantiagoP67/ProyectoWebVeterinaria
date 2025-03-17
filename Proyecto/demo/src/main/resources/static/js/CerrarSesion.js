c5892abce1c8d9265f7e3c51d039ed168f4f2de3
Dani2044 <castro-df@javeriana.edu.co>
2025-03-17 18:46:14 -0500

    Diseño

--- Proyecto/demo/src/main/resources/static/js/CerrarSesion.js
+++ Proyecto/demo/src/main/resources/static/js/CerrarSesion.js
@@ -13,3 +13,36 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
   
+  function openLoginModal() {
+    document.getElementById("loginModal").style.display = "block";
+  }
+
+  function closeLoginModal() {
+    document.getElementById("loginModal").style.display = "none";
+  }
+
+  function openTab(evt, tabName) {
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    let tablinks = document.getElementsByClassName("tablink");
+    for (let i = 0; i < tablinks.length; i++) {
+      tablinks[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+  }
+
+  function redirigirAFormulario() {
+    window.location.href = "crear_mascota.html"; // Redirige a la nueva página
+  }
+  function editarMascota(id) {
+    window.location.href = `editar_mascota.html?id=${id}`;
+  }
+  function redirigirAEditar(id) {
+    // Redirige a la página editar_info_usuario.html y pasa el ID como parámetro
+    window.location.href = "editar_info_usuario.html?id=" + id;
+  }
